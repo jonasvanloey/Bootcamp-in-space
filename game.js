ecd5f3bd0251d1ccc5d3f68e62690717231b260c
TomVerschueren <tom.bassman@hotmail.com>
2017-03-07 09:27:15 +0100

    Beweging af

--- game.js
+++ game.js
@@ -2,14 +2,12 @@ var game = new Phaser.Game(400,600,Phaser.AUTO);
 var rocket;
 var background;
 var bullet;
-<<<<<<< HEAD
 var move;
-
-=======
 var astroids;
 var astroidrnd;
 var timer;
->>>>>>> origin/master
+var touch;
+
 var Preload =
 {
 	preload: function() {
@@ -27,14 +25,16 @@ var PlayGame =
 {
 	create: function()
 	{
-<<<<<<< HEAD
         game.physics.startSystem(Phaser.Physics.ARCADE);
         background = game.add.tileSprite(0,0,400,600,'background');
         rocket = game.add.sprite(171,520,'rocket');
         
+        astroids = game.add.group();
+        game.time.events.loop(Phaser.Timer.SECOND*3, this.RandomAstroid, this);
+        
         game.physics.arcade.enable(rocket);
         
-        game.input.onDown.add(moveIsTrue);
+        game.input.onDown.add(moveIsTrue, this);
         
         game.input.onUp.add(moveIsFalse);
 	},
@@ -42,65 +42,55 @@ var PlayGame =
 	{
         if(move && game.input.activePointer.positionDown.y > 500)
         {
-            console.log(move);
-            Movement(this);
+            console.log();
+            Movement(touch);
         }
         else
         {
             move = false;
         }
 		/*background.tilePosition.y += 4;*/
-        if(move = false)
+        if(!move && rocket.body.velocity.x != 0)
         {
-            rocket.body.velocity.x = 0;
+            if(rocket.body.velocity.x < 0)
+                {
+                rocket.body.velocity.x += 10;
+                }
+            else if(rocket.body.velocity.x > 0)
+                {
+                    rocket.body.velocity.x -= 10;
+                }
         }
-        if(rocket.position.x <= 0)
+        if(rocket.position.x == 0)
         {
             rocket.body.velocity.x = 0;    
         }
-        if(rocket.position.x >= 343)
+        if(rocket.position.x == 343)
         {
             rocket.body.velocity.x = 0;    
         }
-=======
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-        background = game.add.tileSprite(0,0,400,600,'background');
-        rocket = game.add.sprite(171,520,'rocket');
-        astroids = game.add.group();
-        
+        if(touch != null)
+            {
+        if(rocket.position.x+28 < touch.x+15 && rocket.position.x+28 > touch.x-15)
+            {
+                rocket.body.velocity.x = 0;
+            }
+            }
 
-       
-        game.time.events.loop(Phaser.Timer.SECOND*3, this.RandomAstroid, this);
-
-       
-		
-
-		
-	},
-	update: function()
-	{
-
-		if(timer=0){
-			console.log("timer is 0");
-			
-		}
-		/*console.log(timer);
-		/*background.tilePosition.y += 4;*/
-	},
+		},
 	RandomAstroid: function(){
 		randomX = game.rnd.integerInRange(-80,400);
 		astroidrnd = astroids.create(randomX,0,'astroid');
 		astroidrnd.scale.setTo(game.rnd.realInRange(0.4,1.2))
 		game.physics.arcade.enable(astroidrnd);
 		astroidrnd.body.velocity.setTo(0,300);
-
->>>>>>> origin/master
 	}
 };
 
-function moveIsTrue()
+function moveIsTrue(pointer)
 {
     move = true;
+    touch = pointer;
 }
 
 function moveIsFalse()
@@ -110,15 +100,13 @@ function moveIsFalse()
 
 function Movement(pointer)
 {
-    console.log(pointer.x);
-            if(pointer.x > rocket.position.x)
+            if(pointer.x > rocket.position.x+28)
                 {
-                    rocket.body.velocity.x = 150; 
+                    rocket.body.velocity.x = 250; 
                 }
-            else if(pointer.x < rocket.position.x)
+            else if(pointer.x < rocket.position.x+28)
                 {
-                    rocket.body.velocity.x = 150;
-                    console.log(rocket.velocity.x);
+                    rocket.body.velocity.x = -250;
                 }
 }
 
